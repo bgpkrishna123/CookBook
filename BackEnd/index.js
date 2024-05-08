cb6e1a974bb95e6741889074dd7d210338db1e6b
Thiruvalappagari Sumithra <sumithrat49@gmail.com>
2024-05-08 21:45:26 +0530

    user

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,11 +1,25 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const authRouter = require("./routes/User");
+
+const auth = require("./middleware/auth");
 require("dotenv").config();
-const express = require('express');
+const connectToDB = require("./config/db");
+
 const app = express();
+const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Welcome to Home Page!');
-});
+app.use(bodyParser.json());
 
-app.listen(3000, () => {
-  console.log('Server is running at 3000');
+app.use("/users", authRouter);
+
+app.listen(port, () => {
+  try {
+    connectToDB(process.env.DB_URL);
+    console.log("we are connecte to database successfully");
+    console.log(`server is runnin at http://localhost:${port} `);
+  } catch (err) {
+    console.log(err.message);
+  }
 });
