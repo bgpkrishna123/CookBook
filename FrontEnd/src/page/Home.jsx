303696ef3167e441b791179c99d765b3ea2a21c2
bgpkrishna123 <bgpkrishna123@gmail.com>
2024-05-09 22:45:01 +0530

    login-signup

---
+++ FrontEnd/src/page/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "../Components/Navbar";
+import Recipes from "../Components/Recipes";
+import Footer from "../Components/Footer";
+
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <Recipes />
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
